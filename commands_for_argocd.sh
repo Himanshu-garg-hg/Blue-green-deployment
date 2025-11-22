05b870137be276c183d263014e7eded61780b48a
HP <himanshu951garg@gmail.com>
2025-11-22 15:18:14 +0530

    aaaa

--- commands_for_argocd.sh
+++ commands_for_argocd.sh
@@ -1,11 +1,9 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-
+### TO ACCESS ARGOCD SERVER FROM OUTSIDE THE CLUSTER
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 
-
-
+kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}'
 ### TO GET password
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
