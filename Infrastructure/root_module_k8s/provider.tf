eececa3b89e4196af4bf8686411a8feb4fb546ab
HP <himanshu951garg@gmail.com>
2025-11-22 13:18:14 +0530

    aa

--- Infrastructure/root_module_k8s/provider.tf
+++ Infrastructure/root_module_k8s/provider.tf
@@ -1,10 +1,17 @@
 terraform {
+  backend "azurerm" {
+    storage_account_name = "donotdeletestorage"
+    resource_group_name = "donotdelete"
+    container_name = "donotdeletecontainer"
+    key = "donotdelte.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.49.0"
     }
   }
+  
 }
 
 provider "azurerm" {
