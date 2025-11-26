f2263f750f35b38d043713e00ed0e5ccee331a3e
HP <himanshu951garg@gmail.com>
2025-11-26 12:32:44 +0530

    aaa

--- Infrastructure/child_module_k8/main_k8.tf
+++ Infrastructure/child_module_k8/main_k8.tf
@@ -46,7 +46,7 @@ resource "azurerm_key_vault" "kv" {
   resource_group_name         = azurerm_resource_group.rg.name
   sku_name                    = "standard"
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 0
+  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   rbac_authorization_enabled  = true
 }
