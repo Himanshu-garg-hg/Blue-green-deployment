70ae04a255529d1119e07daba53b1f0b1461ecae
HP <himanshu951garg@gmail.com>
2025-11-26 13:37:47 +0530

    aaa

--- commands_for_keyvault.sh
+++
@@ -1,7 +0,0 @@
-az aks get-credentials --resource-group devrg-k8s-infra --name devtodo-aks-cluster --overwrite-existing
-
-az aks enable-addons  --addons azure-keyvault-secrets-provider   --resource-group devrg-k8s-infra   --name devtodo-aks-cluster
-
-
-
-
